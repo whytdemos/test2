2d2b3ced27babb8ec53067e56e953fb2f6c99d4c
whytdemos <34280318+whytdemos@users.noreply.github.com>
2018-11-30 15:06:06 +0000

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: whyt-test2
+# about: This should add an element to the menu
+# version: 0.1
+# authors: jake
+# url: https://github.com/whytdemos/test
+register_custom_html(extraNavItem: "<li id='faq-menu-item'><a href='/faq'>This is a test element</a>
